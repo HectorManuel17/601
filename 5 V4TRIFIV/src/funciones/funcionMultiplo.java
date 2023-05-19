423ca5c0a931a81787f189df961919b0f4a8d278
unknown <espinosapenagoshectormanuel@gmail.com>
2023-05-19 11:22:26 -0600

    Se sube la clase funcionMultiplo

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiplo.java
@@ -0,0 +1,16 @@
+
+package funciones;
+
+
+public class funcionMultiplo {
+    public String retornaMultiplo(int numero){
+    if(numero % 3 == 0 && numero % 5 == 0){
+            return "TRIFIV";
+        } else if (numero % 3 == 0){
+            return "TRI";
+        } else if (numero % 5 == 0){
+            return "FIV";
+        } 
+        return "Ningun valor";
+    }
+}
