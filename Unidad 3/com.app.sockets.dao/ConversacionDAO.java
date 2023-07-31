bafd9a862fb7fe2505e36f37745e4e4159e9f678
HectorManuel17 <133676714+HectorManuel17@users.noreply.github.com>
2023-07-31 09:04:30 -0600

    Add files via upload

---
+++ Unidad 3/com.app.sockets.dao/ConversacionDAO.java
@@ -0,0 +1,10 @@
+package com.app.sockets.dao;
+
+public interface ConversacionDAO {
+
+    //creamos metodos
+    public void registrarConversacionA(String mensaje);
+
+    public void registrarConversacionB(String mensaje);
+
+}
